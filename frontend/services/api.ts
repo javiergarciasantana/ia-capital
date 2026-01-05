dbe44cbcb5bb610de6204af458f848fba0748974
javiersantana <javier.santana.12@ull.edu.es>
2026-01-05 12:08:20 +0000

    changed api.ts

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify'; // Or your preferred notification library
 
 const api = axios.create({
-  baseURL: process.env.NEXT_API_PROXY_ORIGIN,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 // Function to handle logout logic
